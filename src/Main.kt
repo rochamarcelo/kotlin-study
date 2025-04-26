d22f1394c69693b7e9fb3cf942776d3e44dce81a
Marcelo Rocha <contato@omarcelo.com.br>
2025-04-25 22:06:01 -0300

    doc: nullable

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,31 @@
 
 
 fun main() {
+// neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+//    neverNull = null; uncomment this to see the error
+
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+
+    // This is OK
+    nullable = null
+
+    // By default, null values aren't accepted
+    var inferredNonNull = "The compiler assumes non-nullable"
+
+    // Throws a compiler error
+//    inferredNonNull = null; uncomment this to see the error
+
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) // 18
+    // Throws a compiler error
+//    println(strLength(nullable)); uncomment this to see the error
 
 }
