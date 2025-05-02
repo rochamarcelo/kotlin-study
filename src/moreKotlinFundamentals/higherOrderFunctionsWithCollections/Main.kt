50a1d9073195f7f8effd94c73f363f2bb67a9a84
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-02 20:53:58 -0300

    collections -> fold

--- src/moreKotlinFundamentals/higherOrderFunctionsWithCollections/Main.kt
+++ src/moreKotlinFundamentals/higherOrderFunctionsWithCollections/Main.kt
@@ -76,4 +76,8 @@ fun main() {
     crunchyMenu.forEach {
         println("${it.name} - $${it.price}")
     }
+    val totalPrice = cookies.fold(0.0) {total, cookie ->
+        total + cookie.price
+    }
+    println("Total price: $${totalPrice}")
 }
