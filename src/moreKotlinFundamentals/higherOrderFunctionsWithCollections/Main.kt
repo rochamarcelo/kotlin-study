cdc05899e2c011f2c145eae292fc6603abf85981
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-02 20:45:39 -0300

    collections -> groupBy

--- src/moreKotlinFundamentals/higherOrderFunctionsWithCollections/Main.kt
+++ src/moreKotlinFundamentals/higherOrderFunctionsWithCollections/Main.kt
@@ -63,4 +63,17 @@ fun main() {
     softBakedMenu.forEach {
         println("${it.name} - $${it.price}")
     }
+
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu2 = groupedMenu[true] ?: listOf()
+    val crunchyMenu = groupedMenu[false] ?: listOf()
+
+    println("Soft cookies:")
+    softBakedMenu2.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
 }
