95f3dae7e5b55c33b39e881c7de63ed18ae0b7d0
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-02 20:42:22 -0300

    collections -> filter

--- src/moreKotlinFundamentals/higherOrderFunctionsWithCollections/Main.kt
+++ src/moreKotlinFundamentals/higherOrderFunctionsWithCollections/Main.kt
@@ -55,4 +55,12 @@ fun main() {
     fullMenu.forEach {
         println(it)
     }
+
+    val softBakedMenu = cookies.filter {
+        it.softBaked
+    }
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
 }
